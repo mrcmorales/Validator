88661faf6e9f55c3b8d26c34d57085f26f1d4c81
mmoreram <marc.morera@befactory.es>
2014-01-07 13:37:35 +0100

    Validation new features. wip

--- Constraints/AllValidator.php
+++ Constraints/AllValidator.php
@@ -12,35 +12,17 @@
 namespace Symfony\Component\Validator\Constraints;
 
 use Symfony\Component\Validator\Constraint;
-use Symfony\Component\Validator\ConstraintValidator;
-use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+use Symfony\Component\Validator\Constraints\EachValidator;
 
 /**
  * @author Bernhard Schussek <bschussek@gmail.com>
  *
  * @api
+ *
+ * @deprecated Deprecated in 2.5, to be removed in 3.0. Use
+ *             {@link \Symfony\Component\Validator\Constraints\EachValidator} instead.
  */
-class AllValidator extends ConstraintValidator
+class AllValidator extends EachValidator
 {
-    /**
-     * {@inheritDoc}
-     */
-    public function validate($value, Constraint $constraint)
-    {
-        if (null === $value) {
-            return;
-        }
 
-        if (!is_array($value) && !$value instanceof \Traversable) {
-            throw new UnexpectedTypeException($value, 'array or Traversable');
-        }
-
-        $group = $this->context->getGroup();
-
-        foreach ($value as $key => $element) {
-            foreach ($constraint->constraints as $constr) {
-                $this->context->validateValue($element, $constr, '['.$key.']', $group);
-            }
-        }
-    }
 }
